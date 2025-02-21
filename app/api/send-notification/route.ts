0b31d478ddbeba7e53f9ecd744b4481aa34b1906
kotaro1574 <shake1574@gmail.com>
2025-02-21 10:44:48 +0900

    update

--- app/api/send-notification/route.ts
+++ app/api/send-notification/route.ts
@@ -3,6 +3,8 @@ import webpush from "web-push"
 
 import { NotificationPayload, PushSubscription } from "@/types/webpush"
 
+export const runtime = "edge"
+
 export async function POST(request: Request) {
   try {
     const payload = (await request.json()) as {
