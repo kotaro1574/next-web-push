6692dd64385885146775cbba3d7dd4f9ec98076f
kotaro1574 <shake1574@gmail.com>
2025-02-26 11:34:35 +0900

    Revert "update"
    This reverts commit 09ac168cf2b4e22e22c0f1ea72c11f08a394659a.

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,10 @@ import nextPWA from "next-pwa"
 
 const withPWA = nextPWA({
   dest: "public",
-  importScripts: ["/sw.js"],
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  customWorkerDir: "serviceworker",
 })
 
 const nextConfig = withPWA({
