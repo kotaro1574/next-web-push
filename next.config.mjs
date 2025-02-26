f727856c4c666d232c0c426c4985920e36fac962
kotaro1574 <shake1574@gmail.com>
2025-02-26 11:43:27 +0900

    delete: next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-import nextPWA from "next-pwa"
-
 /** @type {import('next').NextConfig} */
 
-const withPWA = nextPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-})
-
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-})
-
+}
 export default nextConfig
