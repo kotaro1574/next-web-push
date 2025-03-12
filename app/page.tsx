ab780081813166ec057ea6f9851ddcacbb437fae
kotaro1574 <shake1574@gmail.com>
2025-03-12 17:54:32 +0900

    fix: runtime指定

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { PushNotificationManager } from "@/components/push-notification-manager"
 
+export const runtime = "edge"
+
 export default function Page() {
   return (
     <div className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
