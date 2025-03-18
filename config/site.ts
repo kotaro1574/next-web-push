320bffcd89a29d6707002193dc696bc4bafbce14
kotaro1574 <shake1574@gmail.com>
2025-03-18 17:27:20 +0900

    update: siteConfig修正

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "next-web-push",
+  description: "nextjsでwebプッシュを実装してみた",
   mainNav: [
     {
       title: "Home",
